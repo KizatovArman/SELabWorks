7cb3643b1fc89909750e45aaa63afd257826a2d0
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2019-02-21 17:55:44 +0600

    17:55

--- SELabWork1/src/Database.java
+++ SELabWork1/src/Database.java
@@ -14,15 +14,15 @@ public class Database {
                 String[] itms = input.split(" ");
                 if(itms[2].equals("100")) {
                     Test.marwin.getItems().add(new Item(itms[0], Integer.parseInt(itms[1]),Integer.parseInt(itms[2]),Double.parseDouble(itms[3])));
-                    System.out.println(itms[0] + itms[1] +itms[2] + itms[3]);
+                    //System.out.println(itms[0] + itms[1] +itms[2] + itms[3]);
                 }
                 if(itms[2].equals("101")) {
                     Test.steam.getItems().add(new Item(itms[0], Integer.parseInt(itms[1]),Integer.parseInt(itms[2]),Double.parseDouble(itms[3])));
-                    System.out.println(itms[0] + itms[1] +itms[2] + itms[3]);
+                    //System.out.println(itms[0] + itms[1] +itms[2] + itms[3]);
                 }
                 if(itms[2].equals("110")){
                     Test.kimex.getItems().add(new Item(itms[0], Integer.parseInt(itms[1]),Integer.parseInt(itms[2]),Double.parseDouble(itms[3])));
-                    System.out.println(itms[0] + itms[1] +itms[2] + itms[3]);
+                    //System.out.println(itms[0] + itms[1] +itms[2] + itms[3]);
                 }
 
                 input=bufferedReader.readLine();
